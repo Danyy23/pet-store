495127cbb95732ab6ed70eb0ee1895f1840e0e5d
Daniel <danielpiarpusan23@gmail.com>
2025-04-10 20:31:24 -0500

    Frontend: SignUp form, Backend: Hash password in signup request

--- src/config/database.php
+++ src/config/database.php
@@ -1,11 +1,11 @@
 <?php
-    
+  /*  
     $host        ="aws-0-us-east-1.pooler.supabase.com";
     $port        ="6543";
     $dbname      ="petstore";
     $user        ="postgres.kusmtronuvgswpgwmcdy";
     $password    ="unicesmag@@";
-/*
+*/
 
     $host        ="localhost";
     $port        ="5432";
@@ -13,7 +13,7 @@
     $user        ="postgres";
     $password    ="unicesmag";
 
-  */
+  
 
     $data_connection="
         host=$host
